bce3b41623798ebf37ad6b96d2a43763e33cc3cf
Volkan Sungar <111195273+volkansungar@users.noreply.github.com>
2025-06-05 11:08:16 +0300

    Update lexer.h

--- PROJECT2/lexer.h
+++ PROJECT2/lexer.h
@@ -8,13 +8,24 @@
 typedef enum {
     TOKEN_EOF = 0,
     TOKEN_IDENTIFIER,
-    TOKEN_KEYWORD,
-    TOKEN_INTEGER,
-    TOKEN_OPERATOR,
-    TOKEN_OPENB,
-    TOKEN_CLOSEB,   
+    TOKEN_WRITE,
+    TOKEN_AND,
+    TOKEN_REPEAT,
+    TOKEN_NEWLINE,
+    TOKEN_TIMES,
+    TOKEN_NUMBER,       // Re-added: "number" keyword for type declaration
+    TOKEN_INTEGER,      // For integer literals (e.g., 123)
+    TOKEN_ASSIGN,       // :=
+    TOKEN_PLUS_ASSIGN,  // +=
+    TOKEN_MINUS_ASSIGN, // -=
+    TOKEN_OPENB,        // {
+    TOKEN_CLOSEB,       // }
     TOKEN_STRING,
-    TOKEN_EOL,
+    TOKEN_EOL,          // ;
+    TOKEN_PLUS,         // +
+    TOKEN_STAR,         // *
+    TOKEN_LPAREN,       // (
+    TOKEN_RPAREN,       // )
     TOKEN_ERROR
 } TokenType;
 
@@ -36,9 +47,12 @@ typedef struct {
     } value;
 } Token;
 
-Token* lexer(FILE* inputFile, char* input_filename);
-
-
-
+// Function declarations for the lexer
+Token* lexer(FILE* inputFile, char* input_filename,int* num_tokens_out);
+void print_token(Token token);
+// Function to get the string representation of a token type
+const char* token_type_str(TokenType type); // <-- ADDED THIS PROTOTYPE
+// Function to free resources allocated by the lexer context
+void free_lex_context(void* ctx_ptr);
 
 #endif // LEXER_H
